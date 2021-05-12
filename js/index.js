9be88b772ba2701a5011eb6ccc54c5d790f42a31
4boysdontcry <hgy02166@gmail.com>
2021-05-12 16:48:32 +0900

    지도날씨 완료

--- js/index.js
+++ js/index.js
@@ -79,7 +79,7 @@ $(function() {
 		r.city.forEach(function(v, i) {
 			var content = '';
 			content += '<div class="co-wrapper '+(v.minimap ? '' : 'minimap')+'" data-lat="'+v.lat+'" data-lon="'+v.lon+'">';
-			content += '<div class="co-wrap '+(v.name == '독도' || v.name == '울릉도' ? 'dokdo': '')+'">';
+			content += '<div class="co-wrap '+(v.name == '독도' || v.name == '울릉도' ? 'dokdo' : '')+'">';
 			content += '<div class="icon-wrap">';
 			content += '<img src="'+defPath+'" class="icon w-100">';
 			content += '</div>';
@@ -96,10 +96,10 @@ $(function() {
 					yAnchor: v.anchor ? v.anchor.y : 0.65,
 			});
 			customOverlay.setMap(map);
+			$(customOverlay.a).mouseenter(onOverlayEnter);
+			$(customOverlay.a).mouseleave(onOverlayLeave);
+			$(customOverlay.a).click(onOverlayClick);
 		});
-		$('.co-wrapper').mouseenter(onOverlayEnter);
-		$('.co-wrapper').mouseleave(onOverlayLeave);
-		$('.co-wrapper').click(onOverlayClick);
 		$(window).trigger('resize');
 	}
 
@@ -109,38 +109,39 @@ $(function() {
 		map.setCenter(new kakao.maps.LatLng(lat, mapCenter.lon));
 		if(windowHeight < 800) {
 			$('.minimap').hide();
-			$('.map-wrapper .co-wrapper').addClass('active')
+			$('.map-wrapper .co-wrapper').addClass('active');
 			map.setLevel(14);
 		}
 		else {
 			map.setLevel(13);
 			$('.minimap').show();
-			$('.map-wrapper .co-wrapper').removeClass('active')
+			$('.map-wrapper .co-wrapper').removeClass('active');
 		}
 	}
 	
 	
 	/*************** 이벤트 등록 *****************/
-	function onOverlayClick(){
-	
+	function onOverlayClick() {
+		// console.log(this)
 	}
 
-	function onOverlayEnter(){
-		// this = co-wrapper 중 호버당한 놈
+	function onOverlayEnter() {
+		// this => .co-wrapper중 호버당한 넘 부모(kakao가 생성한 넘)
 		$(this).find('.co-wrap').css('display', 'flex');
-		$(this).parent().css('z-index', '1');
-		sendData.lat = $(this).data('lat');		// data-lat
-		sendData.lon = $(this).data('lon');		//data- lon
-		$.get(dailyURL, sendData, onload.bind(this))
-		function onload(r){
+		$(this).css('z-index', 1);
+		sendData.lat = $(this).find('.co-wrapper').data('lat');	// data-lat
+		sendData.lon = $(this).find('.co-wrapper').data('lon');	// data-lon
+		$.get(dailyURL, sendData, onLoad.bind(this));
+		function onLoad(r) {
+			// console.log(r);
 			$(this).find('.temp').text(r.main.temp);
-			$(this).find('.icon').attr('src', getIcon(r.weather[0].icon));		// ajax 통신
+			$(this).find('.icon').attr('src', getIcon(r.weather[0].icon));
 		}
 	}
-
-	function onOverlayLeave(){
-		$(this).parent().css('z-index', '0')
-		$(this).find('.co-wrap').css('display', 'none')
+	
+	function onOverlayLeave() {
+		$(this).css('z-index', 0);
+		$(this).find('.co-wrap').css('display', 'none');
 	}
 
 
